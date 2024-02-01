03b55ae7770a44ff53d537b1207a4cf3693afca7
sophiane15 <153296722+sophiane15@users.noreply.github.com>
2024-02-01 22:12:52 +0100

    Add files via upload

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cf0ms0ssmuzp.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '1991.Sof',
+    DB_DATABASE : 'webappdb'
+});
